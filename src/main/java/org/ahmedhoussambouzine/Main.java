a28d344e34340bcae7b3457eac8cbe58b47594aa
AhmedHoussamBouzine <ahmedhoussam.bouzine.01@gmail.com>
2025-01-13 00:44:51 +0100

    adding a loop that reads user input then prints the result

--- src/main/java/org/ahmedhoussambouzine/Main.java
+++ src/main/java/org/ahmedhoussambouzine/Main.java
@@ -1,7 +1,16 @@
 package org.ahmedhoussambouzine;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        while (true) {
+            System.out.print("$ ");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if(input.equals("exit")) {
+                break;
+            }
+        }
     }
 }
