1a111283bf4476a4e702b6630b3d3cce31a02572
AhmedHoussamBouzine <ahmedhoussam.bouzine.01@gmail.com>
2025-01-14 22:43:33 +0100

    adding type command

--- src/main/java/org/ahmedhoussambouzine/Main.java
+++ src/main/java/org/ahmedhoussambouzine/Main.java
@@ -1,18 +1,29 @@
 package org.ahmedhoussambouzine;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner;
+        String input, type;
+        String[] commands = {"echo","type","exit"};
         while (true) {
             System.out.print("$ ");
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
-            if(input.equals("exit")) {
+            scanner = new Scanner(System.in);
+            input = scanner.nextLine();
+            if(input.equals("exit 0") || input.equals("exit")) {
                 break;
             }
-            if(input.startsWith("echo")) {
+            else if(input.startsWith("echo")) {
                 System.out.println(input.substring(5));
+            }else if(input.startsWith("type")) {
+                type = input.substring(5);
+                if(Arrays.asList(commands).contains(type)) {
+                    System.out.println(type+ " is a shell builtin");
+                }else{
+                    System.out.println(type+ ": not found");
+                }
             }else{
                 System.out.println(input + ": command not found");
             }
